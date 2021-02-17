cc60d9e3d04a4492d2edebb338553f39af622f71
Hardik Shukla <49471504+HardU99@users.noreply.github.com>
2021-02-17 08:46:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from  import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
